930f2dc1a549f58b34a3931151a7581bca3f45c2
Skycat1983 <hlaoutaris@gmail.com>
2025-06-11 21:09:48 +0200

    Add new icons and create SystemSlice for state management

---
+++ src/store/systemSlice.ts
@@ -0,0 +1,21 @@
+import type { BaseStoreState, SetState } from "../types/storeTypes";
+
+export type OS = "mac" | "windows";
+
+export interface SystemState {
+  os: OS;
+}
+
+interface SystemActions {
+  toggleOS: () => void;
+}
+
+export type SystemSlice = SystemState & SystemActions;
+
+export const createSystemSlice = (
+  set: SetState<BaseStoreState>
+): SystemSlice => ({
+  os: "mac",
+  toggleOS: () =>
+    set((state) => ({ os: state.os === "mac" ? "windows" : "mac" })),
+});
