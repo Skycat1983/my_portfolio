2037d35133731d61f30488a68f6973f00342fa6e
Skycat1983 <hlaoutaris@gmail.com>
2025-06-12 19:51:34 +0200

    fixed import paths

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,84 +1,141 @@
-export const BACKGROUND_OS = "src/assets/wallpapers/fuji.jpg";
-export const BACKGROUND_MS = "src/assets/wallpapers/windows.png";
+// Background images
+import BACKGROUND_OS from "../assets/wallpapers/fuji.jpg";
+import BACKGROUND_MS from "../assets/wallpapers/windows.png";
 
-// PROFILE ICONS
-const GITHUB = "src/assets/icons_m/github.png";
-const LINKEDIN = "src/assets/icons/linkedin.png";
+// Profile icons
+import GITHUB from "../assets/icons_m/github.png";
+import LINKEDIN from "../assets/icons/linkedin.png";
+import LINKEDIN_SMALL from "../assets/icons_s/linked_in1.png";
+import GITHUB_SMALL from "../assets/icons_s/github_white.png";
 
-const LINKEDIN_SMALL = "src/assets/icons_s/linked_in1.png";
-const GITHUB_SMALL = "src/assets/icons_s/github_white.png";
+// Folders
+import FOLDER from "../assets/icons_m/folder_os.png";
+import BIN_EMPTY from "../assets/icons_m/bin_empty.png";
+import BIN_FULL from "../assets/icons_m/bin_full.png";
+import FOLDER_OS from "../assets/icons_m/folder_os.png";
+import FOLDER_MS from "../assets/icons_m/folder_ms.png";
 
-// ICONS
-const ICON = "src/assets/icons_m/1.png";
+// Apps
+import TERMINAL from "../assets/icons_m/terminal.png";
+import FIREFOX from "../assets/icons_m/firefox.png";
+import EDGE from "../assets/icons_m/edge.png";
 
-// FOLDERS
-export const FOLDER = "src/assets/icons_m/folder_os.png";
-export const BIN_EMPTY = "src/assets/icons_m/bin_empty.png";
-export const BIN_FULL = "src/assets/icons_m/bin_full.png";
+// Stack icons
+import REACT from "../assets/icons_m/react.png";
+import TYPESCRIPT from "../assets/icons_m/typescript.png";
+import NODEJS from "../assets/icons_m/nodejs.png";
+import MONGODB from "../assets/icons_m/mongo_db.png";
+import EXPRESS from "../assets/icons_m/express.png";
+import JAVASCRIPT from "../assets/icons_m/javascript.png";
+import JEST from "../assets/icons_m/jest.png";
+import POSTMAN from "../assets/icons_m/postman.png";
+import TAILWIND from "../assets/icons_m/tailwind.png";
+import FIREBASE from "../assets/icons_m/firebase.png";
+import HTML from "../assets/icons_m/html.png";
+import CSS from "../assets/icons_m/css.png";
+import NEXT1 from "../assets/icons_m/next1.png";
+import NEXT2 from "../assets/icons_m/next2.png";
 
-export const FOLDER_OS = "src/assets/icons_m/folder_os.png";
-export const FOLDER_MS = "src/assets/icons_m/folder_ms.png";
+// Easter eggs
+import EASTER_EGG1 from "../assets/icons_m/egg1.png";
+import EASTER_EGG2 from "../assets/icons_m/egg2.png";
+import EASTER_EGG3 from "../assets/icons_m/egg3.png";
+import EGG_BROKEN from "../assets/icons_m/egg4.png";
 
-// APPS
-export const TERMINAL = "src/assets/icons_m/terminal.png";
+// Other icons
+import PDF from "../assets/icons_m/pdf.png";
+import APPLE from "../assets/icons_m/apple.png";
+import JL1 from "../assets/icons_m/JL1.png";
+import JL2 from "../assets/icons_m/JL2.png";
+import IMAGE1 from "../assets/icons_m/image1.png";
+import IMAGE2 from "../assets/icons_m/image2.png";
+import JOSEPH_LAOUTARIS from "../assets/icons_m/joseph_laoutaris.png";
 
-// STACK ICONS
-const REACT = "src/assets/icons_m/react.png";
-const TYPESCRIPT = "src/assets/icons_m/typescript.png";
-const NODEJS = "src/assets/icons_m/nodejs.png";
-const MONGODB = "src/assets/icons_m/mongo_db.png";
-const EXPRESS = "src/assets/icons_m/express.png";
-const JAVASCRIPT = "src/assets/icons_m/javascript.png";
-const JEST = "src/assets/icons_m/jest.png";
-const POSTMAN = "src/assets/icons_m/postman.png";
-const TAILWIND = "src/assets/icons_m/tailwind.png";
-const FIREBASE = "src/assets/icons_m/firebase.png";
-const EASTER_EGG1 = "src/assets/icons_m/egg1.png";
+// Logos
+import LOGO_MAC from "../assets/icons_m/logo_mac.png";
+import LOGO_MS from "../assets/icons_m/logo_ms.png";
 
-const EASTER_EGG2 = "src/assets/icons_m/egg2.png";
-const EASTER_EGG3 = "src/assets/icons_m/egg3.png";
-const EGG_BROKEN = "src/assets/icons_m/egg4.png";
+// Folders (additional)
+import FOLDER2 from "../assets/icons_m/folder2.png";
+import FOLDER3 from "../assets/icons_m/folder3.png";
+import FOLDER4 from "../assets/icons_m/folder4.png";
+import FOLDER_MS2 from "../assets/icons_m/folder_ms2.png";
 
-export const FIREFOX = "src/assets/icons_m/firefox.png";
-export const EDGE = "src/assets/icons_m/edge.png";
-const PDF = "src/assets/icons_m/pdf.png";
-const APPLE = "src/assets/icons_m/apple.png";
+// Export the background constants
+export { BACKGROUND_OS, BACKGROUND_MS };
 
-const JL = "src/assets/icons_m/jl1.png";
-const IMAGE = "src/assets/icons_m/image1.png";
-
-export const LOGO_MAC = "src/assets/icons_m/logo_mac.png";
-export const LOGO_MS = "src/assets/icons_m/logo_ms.png";
-
-export const images = {
-  LOGO_MAC,
-  LOGO_MS,
-  IMAGE,
-  EGG_BROKEN,
-  JL,
-  APPLE,
-  LINKEDIN_SMALL,
-  GITHUB_SMALL,
-  FIREFOX,
-  GITHUB,
-  LINKEDIN,
-  ICON,
+// Export other commonly used exports
+export {
   FOLDER,
   BIN_EMPTY,
   BIN_FULL,
+  FOLDER_OS,
+  FOLDER_MS,
   TERMINAL,
+  FIREFOX,
+  EDGE,
+  LOGO_MAC,
+  LOGO_MS,
+};
+
+// Export the images object with all assets
+export const images = {
+  // Backgrounds
+  BACKGROUND_OS,
+  BACKGROUND_MS,
+
+  // Profile
+  GITHUB,
+  LINKEDIN,
+  LINKEDIN_SMALL,
+  GITHUB_SMALL,
+
+  // Folders
+  FOLDER,
+  BIN_EMPTY,
+  BIN_FULL,
+  FOLDER_OS,
+  FOLDER_MS,
+  FOLDER2,
+  FOLDER3,
+  FOLDER4,
+  FOLDER_MS2,
+
+  // Apps
+  TERMINAL,
+  FIREFOX,
+  EDGE,
+
+  // Stack
   REACT,
   TYPESCRIPT,
   NODEJS,
   MONGODB,
+  EXPRESS,
   JAVASCRIPT,
   JEST,
   POSTMAN,
   TAILWIND,
   FIREBASE,
+  HTML,
+  CSS,
+  NEXT1,
+  NEXT2,
+
+  // Easter eggs
   EASTER_EGG1,
   EASTER_EGG2,
   EASTER_EGG3,
-  EXPRESS,
+  EGG_BROKEN,
+
+  // Other
   PDF,
+  APPLE,
+  JL1,
+  JL2,
+  IMAGE1,
+  IMAGE2,
+  JOSEPH_LAOUTARIS,
+  LOGO_MAC,
+  LOGO_MS,
 };
