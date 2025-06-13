31c179a52d1633186e2cd281c49d379ccfe585f1
Skycat1983 <hlaoutaris@gmail.com>
2025-06-13 09:33:31 +0200

    Updated MenubarOptions to dynamically display keyboard shortcuts based on OS

--- src/components/menubar/MenubarOptions.tsx
+++ src/components/menubar/MenubarOptions.tsx
@@ -7,89 +7,93 @@ import {
   MenubarTrigger,
 } from "../ui/menubar";
 import { HoverableItem } from "./HoverableItem";
+import { useNewStore } from "../../hooks/useNewStore";
 
 export function MenubarOptions() {
+  const { os } = useNewStore();
+
+  const char = os === "mac" ? "⌘" : "Ctrl ";
   return (
     <Menubar>
       <MenubarMenu>
-        <MenubarTrigger>File</MenubarTrigger>
+        <MenubarTrigger className="!cursor-default">File</MenubarTrigger>
         <MenubarContent>
           <HoverableItem>
-            New <MenubarShortcut>⌘N</MenubarShortcut>
+            New <MenubarShortcut>{char}N</MenubarShortcut>
           </HoverableItem>
           <HoverableItem>
-            Open <MenubarShortcut>⌘O</MenubarShortcut>
-          </HoverableItem>
-          <MenubarSeparator />
-          <HoverableItem>
-            Save <MenubarShortcut>⌘S</MenubarShortcut>
-          </HoverableItem>
-          <HoverableItem>
-            Save As <MenubarShortcut>⇧⌘S</MenubarShortcut>
+            Open <MenubarShortcut>{char}O</MenubarShortcut>
           </HoverableItem>
           <MenubarSeparator />
           <HoverableItem>
-            Close <MenubarShortcut>⌘W</MenubarShortcut>
+            Save <MenubarShortcut>{char}S</MenubarShortcut>
           </HoverableItem>
           <HoverableItem>
-            Print <MenubarShortcut>⌘P</MenubarShortcut>
+            Save As <MenubarShortcut>{char}⇧S</MenubarShortcut>
+          </HoverableItem>
+          <MenubarSeparator />
+          <HoverableItem>
+            Close <MenubarShortcut>{char}W</MenubarShortcut>
+          </HoverableItem>
+          <HoverableItem>
+            Print <MenubarShortcut>{char}P</MenubarShortcut>
           </HoverableItem>
         </MenubarContent>
       </MenubarMenu>
       <MenubarMenu>
-        <MenubarTrigger>Edit</MenubarTrigger>
+        <MenubarTrigger className="!cursor-default">Edit</MenubarTrigger>
         <MenubarContent>
           <HoverableItem>
-            Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+            Undo <MenubarShortcut>{char}Z</MenubarShortcut>
           </HoverableItem>
           <HoverableItem>
-            Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
-          </HoverableItem>
-          <MenubarSeparator />
-          <HoverableItem>
-            Cut <MenubarShortcut>⌘X</MenubarShortcut>
-          </HoverableItem>
-          <HoverableItem>
-            Copy <MenubarShortcut>⌘C</MenubarShortcut>
-          </HoverableItem>
-          <HoverableItem>
-            Paste <MenubarShortcut>⌘V</MenubarShortcut>
+            Redo <MenubarShortcut>{char}⇧Z</MenubarShortcut>
           </HoverableItem>
           <MenubarSeparator />
           <HoverableItem>
-            Select All <MenubarShortcut>⌘A</MenubarShortcut>
+            Cut <MenubarShortcut>{char}X</MenubarShortcut>
+          </HoverableItem>
+          <HoverableItem>
+            Copy <MenubarShortcut>{char}C</MenubarShortcut>
+          </HoverableItem>
+          <HoverableItem>
+            Paste <MenubarShortcut>{char}V</MenubarShortcut>
+          </HoverableItem>
+          <MenubarSeparator />
+          <HoverableItem>
+            Select All <MenubarShortcut>{char}A</MenubarShortcut>
           </HoverableItem>
         </MenubarContent>
       </MenubarMenu>
       <MenubarMenu>
-        <MenubarTrigger>View</MenubarTrigger>
+        <MenubarTrigger className="!cursor-default">View</MenubarTrigger>
         <MenubarContent>
           <HoverableItem>Show Toolbar</HoverableItem>
           <HoverableItem>Show Sidebar</HoverableItem>
           <MenubarSeparator />
           <HoverableItem>
-            Enter Full Screen <MenubarShortcut>⌃⌘F</MenubarShortcut>
+            Enter Full Screen <MenubarShortcut>{char}⌃F</MenubarShortcut>
           </HoverableItem>
           <MenubarSeparator />
           <HoverableItem>
-            Zoom In <MenubarShortcut>⌘+</MenubarShortcut>
+            Zoom In <MenubarShortcut>{char}+</MenubarShortcut>
           </HoverableItem>
           <HoverableItem>
-            Zoom Out <MenubarShortcut>⌘-</MenubarShortcut>
+            Zoom Out <MenubarShortcut>{char}-</MenubarShortcut>
           </HoverableItem>
           <HoverableItem>
-            Actual Size <MenubarShortcut>⌘0</MenubarShortcut>
+            Actual Size <MenubarShortcut>{char}0</MenubarShortcut>
           </HoverableItem>
         </MenubarContent>
       </MenubarMenu>
       <MenubarMenu>
-        <MenubarTrigger>Help</MenubarTrigger>
+        <MenubarTrigger className="!cursor-default">Help</MenubarTrigger>
         <MenubarContent>
           <HoverableItem>About</HoverableItem>
           <HoverableItem>Support</HoverableItem>
           <MenubarSeparator />
           <HoverableItem>
-            Keyboard Shortcuts <MenubarShortcut>⌘/</MenubarShortcut>
+            Keyboard Shortcuts <MenubarShortcut>{char}/</MenubarShortcut>
           </HoverableItem>
           <HoverableItem>Contact Us</HoverableItem>
         </MenubarContent>
